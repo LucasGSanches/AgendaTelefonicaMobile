b08fe2c15e45966d45552f6430919c4e6b94b0f7
Lucas Sanches <165438132+LucasGSanches@users.noreply.github.com>
2026-08-31 08:59:40 -0300

    first lesson

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image} from 'react-native';
+import { useState } from "react";
 
 export default function App() {
+	const [variable, useVariable] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30}}>Contador</Text>
+      <Text style={{ fontSize: 30}}>{variable}</Text>
+      <Button title="Incrementar" onPress={() => useVariable(variable + 1)}/>
+      <Button title="Decrementar" onPress={() => useVariable(variable - 1)}/>
     </View>
   );
 }
